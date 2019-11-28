61e7aa4cef23f2ca4462ce0621c15fa5a488248a
samuel <samuelgarciacompanys@gmail.com>
2019-11-28 17:41:06 +0100

    linters

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -6,32 +6,32 @@ class ButtonPanel extends React.Component {
     return (
       <div>
         <div className="group-1">
-          <button>AC</button>
-          <button>+/-</button>
-          <button>%</button>
-          <button>÷</button>
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button">÷</button>
         </div>
         <div className="group-2">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>X</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button">X</button>
         </div>
         <div className="group-3">
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button">-</button>
         </div>
         <div className="group-4">
-          <button>1</button>
-          <button>2/-</button>
-          <button>3</button>
-          <button>+</button>
+          <button type="button">1</button>
+          <button type="button">2/-</button>
+          <button type="button">3</button>
+          <button type="button">+</button>
         </div>
         <div className="group-5">
-          <button>o</button>
-          <button>.</button>
+          <button type="button">o</button>
+          <button type="button">.</button>
         </div>
       </div>
     );
