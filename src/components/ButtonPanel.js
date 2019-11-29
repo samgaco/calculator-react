702dfb99eb9fa896f516c9b1e030a416da7d8bd3
samuel <samuelgarciacompanys@gmail.com>
2019-11-29 13:46:07 +0100

    buttonpanel passing props to the button component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,43 +1,43 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import Button from './Button';
 
 class ButtonPanel extends React.Component {
   render() {
     return (
       <div>
         <div className="group-1">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button">÷</button>
+          <Button buttonName="AC" />
+          <Button buttonName="+/-" />
+          <Button buttonName="%" />
+          <Button buttonName="÷" />
         </div>
         <div className="group-2">
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button">X</button>
+          <Button buttonName="7" />
+          <Button buttonName="8" />
+          <Button buttonName="9" />
+          <Button buttonName="X" />
+          <Button buttonName="AC" />
         </div>
         <div className="group-3">
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button">-</button>
+          <Button buttonName="4" />
+          <Button buttonName="5" />
+          <Button buttonName="6" />
+          <Button buttonName="-" />
         </div>
         <div className="group-4">
-          <button type="button">1</button>
-          <button type="button">2/-</button>
-          <button type="button">3</button>
-          <button type="button">+</button>
+          <Button buttonName="1" />
+          <Button buttonName="2" />
+          <Button buttonName="3" />
+          <Button buttonName="+" />
         </div>
         <div className="group-5">
-          <button type="button">o</button>
-          <button type="button">.</button>
+          <Button buttonName="0" />
+          <Button buttonName="." />
         </div>
       </div>
     );
   }
 }
-
-ButtonPanel.defaultProps = { result: '0' };
 
 export default ButtonPanel;
