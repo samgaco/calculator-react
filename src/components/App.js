3c36b0a3720c8b44c4c6070f30cd1f3972813c54
samuel <samuelgarciacompanys@gmail.com>
2019-12-05 17:38:56 +0100

    added handleclick in the app file

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -5,14 +7,36 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
+import operate from '../logic/operate';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { total, next, operation } = calculate(this.state, buttonName);
+
+    this.setState({
+      total,
+      next,
+      operation,
+    });
+  }
+
+
   render() {
     return (
       <div className="bodyApp">
-        <Display />
-        <ButtonPanel />
+        <Display total={this.state.total} next={this.state.next} />
+        <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
